4383972ef90f57c8cc417e8ad4a0744d5b977261
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-06 12:21:55 +0300

    adminPanel v1

---
+++ vylka/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fork_Site.Controllers
+{
+    public class AdminController : Controller
+    {
+        //public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        /*public IActionResult Categories()
+        {
+            List<Tbl_Category> allCategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).toList();
+            return View(allCategories);
+        }*/
+    }
+}

e9549279f354e91d7251168e1fb610a0ce04ec1a
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-09 14:06:32 +0300

    js file edited

--- vylka/wwwroot/AdminStyles/js/sb-admin.js
+++ vylka/wwwroot/AdminStyles/js/sb-admin.js
@@ -1,14 +1,12 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
-  // Toggle the side navigation
   $("#sidebarToggle").on('click',function(e) {
     e.preventDefault();
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
   });
 
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
@@ -18,17 +16,6 @@
     }
   });
 
-  // Scroll to top button appear
-  $(document).on('scroll',function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
@@ -37,4 +24,4 @@
     event.preventDefault();
   });
 
-})(jQuery); // End of use strict
+})(jQuery);

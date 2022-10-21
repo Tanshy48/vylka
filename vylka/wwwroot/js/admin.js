0a62db9773c5702578c50e139e69dae0cdeb216d
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-21 20:41:49 +0300

    adminpanel buttons

--- vylka/wwwroot/js/admin.js
+++ vylka/wwwroot/js/admin.js
@@ -1,10 +1,14 @@
 (function($) {
   "use strict";
 
+  //Toggler function of navigation sidebar
   $("#sidebarToggle").on('click',function(e) {
     e.preventDefault();
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
   });
   
+  //To make width of the table title div the same as width of element of the table
+  $(".bg-table-title").css('min-width', $('table').width());
+
 })(jQuery);

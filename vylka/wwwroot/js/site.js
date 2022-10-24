3f92c026aba987cd03f06aa2e6d36cb16a1374aa
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-24 18:17:36 +0300

    admin layout

--- vylka/wwwroot/js/site.js
+++ vylka/wwwroot/js/site.js
@@ -2,6 +2,8 @@
     "use strict";
   
     //To get footer to the end of page
-    $(".render-content").css('min-height', $(window).height()-$("footer").height()-$("nav").height());
+    $(".render-content")
+        .css('min-height', $(window).height()-$("footer").height()-$("nav").height())
+        .css('width','100%');
     
 })(jQuery);

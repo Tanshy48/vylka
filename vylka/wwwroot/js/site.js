8dcab6b7635b63f0fb0a004facf91032cb6ce6d2
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-30 19:04:27 +0200

    empty

--- vylka/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿(function($) {
-    "use strict";
-  
-    //To get footer to the end of page
-    $(".render-content")
-        .css('min-height', $(window).height()-$("footer").height()-$("nav").height())
-        .css('width','100%');
-    
-})(jQuery);

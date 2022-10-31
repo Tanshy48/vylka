d0eec0bb1404416f78733187668b706924913a86
Tanshy <shyshkanych.tetyana@student.uzhnu.edu.ua>
2022-10-31 23:05:44 +0200

    Create new category and display all categories

--- vylka/Models/CategoryModel.cs
+++ vylka/Models/CategoryModel.cs
@@ -6,6 +6,7 @@ namespace vylka.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public List<ProductModel> Product { get; set; }
     }

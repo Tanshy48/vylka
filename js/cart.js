a1c7cf189f203dd39ff69443b026e5ee34f18794
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-10-31 09:10:00 +0200

    cart index jquery

---
+++ js/cart.js
@@ -0,0 +1,32 @@
+(function($) {
+    "use strict";
+
+    let count = 1;
+    let money = 0;
+    
+    $('.price').each(function() {
+        money += +($(this).html());
+    });
+    $( "#totalAmount" ).html(money)
+
+    $( ".products" ).on('click', '.plus', function(event) {
+        event.preventDefault();
+        count = +$(this).next().html() + 1
+        $(this).next().html(count)
+
+        money+= +($(this).parent().prev().find('.price').html())
+        $( "#totalAmount" ).html(money)
+    })
+    $( ".products" ).on('click', '.minus', function(event) {
+        if(+$(this).prev().html() > 0) {
+            event.preventDefault();
+            count = +$(this).prev().html() - 1
+            $(this).prev().html(count)
+            
+            money-= +($(this).parent().prev().find('.price').html())
+            $( "#totalAmount" ).html(money)
+        }
+        
+    })
+
+})(jQuery);

12bcbc7d3f049700b60525fc95825a549f83ef02
vvu <vasylechko.vlasta@student.uzhnu.edu.ua>
2022-11-08 17:00:45 +0200

    change state btn on products page

--- vylka/wwwroot/js/admin.js
+++ vylka/wwwroot/js/admin.js
@@ -1,23 +1,23 @@
 (function($) {
-  "use strict";
+  'use strict'
 
   //Toggler function of navigation sidebar
-  $("#sidebarToggle").on('click',function(e) {
-    e.preventDefault();
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
+  $('#sidebarToggle').on('click',function(e) {
+    e.preventDefault()
+    $('body').toggleClass('sidebar-toggled')
+    $('.sidebar').toggleClass('toggled')
   });
   
   //To make width of the table title div the same as width of element of the table
-  $(".bg-table-title").css('min-width', $('table').width());
+  $('.bg-table-title').css('min-width', $('table').width());
 
-  $("#delCheckBox").click(function() {
-    if($("#delCheckBox").is(":checked")){
-      $("#sendFormBtn").attr("disabled", false);
+  $('#delCheckBox').click(function() {
+    if($('#delCheckBox').is(':checked')){
+      $('#sendFormBtn').attr('disabled', false)
     } else {
-      $("#sendFormBtn").attr("disabled", true);
+      $('#sendFormBtn').attr('disabled', true)
     }
-    console.log($("#delCheckBox").is(":checked"))
+    console.log($('#delCheckBox').is(':checked'))
   });
 
 })(jQuery);

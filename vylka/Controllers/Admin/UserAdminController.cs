ab06370d8461c475d9dba8fce213bb01e9a5f655
Tanshy <shyshkanych.tetyana@student.uzhnu.edu.ua>
2022-11-15 21:29:31 +0200

    BugFix DeleteUser

--- vylka/Controllers/Admin/UserAdminController.cs
+++ vylka/Controllers/Admin/UserAdminController.cs
@@ -1,6 +1,7 @@
 #nullable disable
 
 using Microsoft.AspNetCore.Mvc;
+using vylka.Areas.Entity;
 using vylka.Areas.Identity.Data;
 
 namespace vylka.Controllers.Admin
@@ -39,11 +40,21 @@ namespace vylka.Controllers.Admin
         {
             var obj = _context.Users.Find(id);
             var cart = _context.Cart.OrderBy(o => o.Id).LastOrDefault(u => u.CartUserId.Id == id);
+            var items = _context.ShippingDetail.Where(u => u.UserId.Id == id);
+            
             if (obj == null || cart == null)
             {
                 return NotFound();
             }
 
+            if (items != null)
+            {
+                foreach (var item in _context.ShippingDetail.Where(u => u.UserId.Id == id))
+                {
+                    _context.ShippingDetail.Remove(item);
+                }
+            }
+            
             _context.Cart.Remove(cart);
             _context.Users.Remove(obj);
             _context.SaveChanges();
